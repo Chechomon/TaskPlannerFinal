fdf07934c89cc08678508640fa403ab7cdab423d
Alejandrito <alejope98@hotmail.com>
2019-10-19 16:02:54 -0500

    segundo y flitro

--- src/TaskCard.js
+++ src/TaskCard.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import {CardContent} from "@material-ui/core";
 import Typography from '@material-ui/core/Typography';
+import {Link}  from "react-router-dom";
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+function update(){
+    return(
+        window.location.assign('/updatetask')
+    );
+}
 
 class TaskCard extends React.Component{
 
@@ -11,19 +19,24 @@ class TaskCard extends React.Component{
     render() {
         return(
           <div className="TaskCard">
-              <Card>
-                  <CardContent>
-                      <Typography variant="h5">
-                          {this.props.description}
-                      </Typography>
-                      <Typography color="textSecondary">
-                          {this.props.status}-DueDate:{this.props.dueDate}
-                      </Typography>
-                      <Typography color="textSecondary">
-                          Responsible: {this.props.responsible}
-                      </Typography>
-                  </CardContent>
-              </Card>
+              <CardActionArea  component={Link} to={{pathname: '/updatetask', state:{
+                          description : this.props.description,
+                          status : this.props.status,
+                          responsible : this.props.responsible,
+                          dueDate : this.props.dueDate
+                      }}}>
+                      <CardContent>
+                          <Typography variant="h5">
+                              {this.props.description}
+                          </Typography>
+                          <Typography color="textSecondary">
+                              {this.props.status}-DueDate:{this.props.dueDate}
+                          </Typography>
+                          <Typography color="textSecondary">
+                              Responsible: {this.props.responsible}
+                          </Typography>
+                      </CardContent>
+                  </CardActionArea>
           </div>
         );
     }
